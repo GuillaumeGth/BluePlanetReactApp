302a20c1314a4785db50789074d8307110c12328
GuillaumeGth <guillaume.zarb@gmail.com>
2020-01-03 19:47:59 -0800

    Google Map integration

--- src/components/GoogleMap/GoogleMap.js
+++ src/components/GoogleMap/GoogleMap.js
@@ -1,25 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import GoogleMapReact from 'google-map-react';
+import style from './style';
+import Marker from './Marker';
 
 const GoogleMap = (props) =>{  
   const MapContainer = styled.div`
     width: 100%;
     height: 500px;
     padding: 0 0 100px 0;`;
+  
 
   const maps = {
-    lat: -5.599116,
-    lng: 120.449722
+    lat: -5.559116,
+    lng: 120.240000
   }
 
   const zoom = 12;  
+  const lat = -5.60285;
+  const lng = 120.4488;
   return (
     <MapContainer>
-      <GoogleMapReact        
-        bootstrapURLKeys="AIzaSyDUBD-j83YUQJIKQphkz1DtWIlDkyAjrVY"
+      <GoogleMapReact  
+        options={{styles: style}}      
+        bootstrapURLKeys={{key: "AIzaSyDUBD-j83YUQJIKQphkz1DtWIlDkyAjrVY"}}
         defaultCenter={maps}
         defaultZoom={zoom}>
+          <Marker lat={lat} lng={lng} />
       </GoogleMapReact>
     </MapContainer>
   )
---
+++ src/components/GoogleMap/Marker/Marker.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Marker.css';
+
+
+const Marker = (props) =>{  
+
+  return (
+    <div>
+      <div className="pin bounce" />
+      <div className="pulse" />
+    </div>)
+}
+
+export default (Marker);
