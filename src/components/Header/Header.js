91da1be0f8ed03040c143f790978ad469f3b9cec
GuillaumeGth <guillaume.zarb@gmail.com>
2020-01-08 10:13:10 -0800

    Separation of concern

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,10 @@
 import React, { useEffect } from 'react';
 import Menu from "./Menu";
-import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAnchor } from '@fortawesome/free-solid-svg-icons';
-import FlexContainer from '../Layout/FlexContainer';
+import {AppName, HeaderControl, Flag, LangContainer} from './style'
 
 const Header = (props) =>{
-
-  const AppName = styled.div`
-    margin: 0 25px;
-    font-size: 1.2rem;
-    letter-spacing: 2px;`;
-
-  const HeaderControl = styled.div`  
-    z-index: 2;
-    background: rgba(0, 0, 0, .2); 
-    position: fixed;  
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-center: flex-start;
-    color: white;
-    transition: .5s;
-
-    &.scrolled {      
-      background: white !important;
-      box-shadow: 0 15px 40px -12px rgba(0, 0, 0, 0.45);
-      & .menu-item-label,
-        ${AppName},
-        .anchor-icon {
-        color: #252627 !important;
-      }
-    }
-  `;
 
   useEffect(() => {
     window.addEventListener('scroll', scrollhandler)
@@ -61,12 +32,6 @@ const Header = (props) =>{
   }
 
   const headerElement = React.createRef();
-
-  const Flag = styled.img`width: 30px; cursor: pointer !important;
-  margin: 0 150px;`;
-
-  const LangContainer = styled(FlexContainer)`
-    margin: 0 150px !important;`;
 
   const flagClickHandler = () => {
     props.langClickHandler(getNewLang());
