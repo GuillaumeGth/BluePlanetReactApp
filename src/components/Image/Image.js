ac28d5eb455031382bf9a335002eda815dc3f63b
GuillaumeGth <guillaume.zarb@gmail.com>
2020-01-18 01:16:37 -0800

    obsolete

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,7 +1,4 @@
 import React from "react";
-import MaterialButton from "@material-ui/core/Button";
-import styled from "styled-components";
-import SendIcon from "@material-ui/icons/Send";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Image = props => {
